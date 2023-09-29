d92dc87450fca4edd63e2b27ddd5fe5e4717e839
itssos <116051861+itssos@users.noreply.github.com>
2023-09-28 21:23:38 -0500

    Delete js directory

--- js/numeros.js
+++
@@ -1,8 +0,0 @@
-// Función para agregar un número al campo de entrada de contraseña
-function appendNumber(number) {
-    var passwordInput = document.getElementById("password");
-    var passwordButtonsInput = document.getElementById("passwordButtons");
-
-    passwordInput.value += number; // Agrega el número al campo de contraseña
-    passwordButtonsInput.value += number; // Agrega el número al campo de botones numéricos
-}
