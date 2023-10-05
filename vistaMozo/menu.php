eaf9232b63656fe4b37cb74663cad168106de52c
JFVela <jfvela_2012@hotmail.com>
2023-10-05 02:06:58 -0500

    Agregue el modal

--- vistaMozo/menu.php
+++ vistaMozo/menu.php
@@ -53,6 +53,66 @@
         <button id="cancelarPedido" style="display: none;">CANCELAR PEDIDO</button>
     </div>
 
+
+    <!-- Button trigger modal -->
+    <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#exampleModal'>ADD +</button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">MENÚ</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <!--TABLA DE MENÚ-->
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Imagen</th>
+                                <th scope="col">Descrip</th>
+                                <th scope="col">Cant</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            require("../Config/conexion.php");
+                            $sql = $conexion->query("SELECT * FROM plato");
+                            while ($resultado = $sql->fetch_assoc()) {
+                            ?>
+                                <tr>
+                                    <td class="text-center align-middle">
+                                        <img src="<?php echo $resultado['imagen']; ?>" alt="Imagen del Plato" style="max-height: 50px;">
+                                    </td>
+                                    <td class="text-center align-middle">
+                                        <p><?php echo $resultado['nombre'] ?></p>
+                                        <p>Costo: s/<?php echo $resultado['precio'] ?></p>
+                                    </td>
+                                    <td class="text-center align-middle">
+                                        <input type="hidden" name="imagen" value="<?php echo $resultado['imagen']; ?>">
+                                        <input type="hidden" name="nombre" value="<?php echo $resultado['nombre'] ?>; ?>">
+                                        <input type="hidden" name="precio" value="<?php echo $resultado['precio'] ?>; ?>">
+                                        <input type="number" name="cantidad" value="0" class="form-control">
+                                        <!-- Cambia el id a una clase para el botón "Agregar a la Orden" -->
+                                        <button type="button" class="btn btn-success agregar-plato-btn">Agregar</button>
+                                    </td>
+                                </tr>
+
+                            <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                    <button type="button" class="btn btn-primary">OK</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <script>
         document.addEventListener("DOMContentLoaded", function() {
             const selectMesas = document.getElementById("mesas");
@@ -125,6 +185,8 @@
 
         });
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
